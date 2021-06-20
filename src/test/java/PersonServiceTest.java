0bbb759c8a915ba3614072db3221f254d6d0d997
Archit Kumar <architkumar85@gmail.com>
2021-06-21 00:41:57 +0530

    added test of the person service and the base service

--- src/test/java/PersonServiceTest.java
+++ src/test/java/PersonServiceTest.java
@@ -1,23 +1,70 @@
+import com.application.person.Person;
 import com.application.person.PersonDao;
 import com.application.person.PersonService;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
+
+import java.util.Collections;
+
+import static com.application.Constants.DEFAULT_PERSON_NAME;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 
 @RunWith(MockitoJUnitRunner.class)
-public class PersonServiceTest {
+public class PersonServiceTest extends BaseTest {
 
+    @Captor
+    ArgumentCaptor<Person> personArgumentCaptor;
+    @Captor
+    ArgumentCaptor<String> stringArgumentCaptor;
+    @InjectMocks
     private PersonService personService;
+    @Mock
+    private PersonDao personDao;
 
     @Before
     public void beforeAll() {
-        this.personService = new PersonService(new PersonDao());
+
     }
 
     @Test
-    public void test() {
-
+    public void addPersons() {
+        String name = "jack";
+        doNothing().when(personDao).addPerson(any());
+        personService.addPersons(Collections.singletonList(name));
+        verify(personDao, times(1)).addPerson(personArgumentCaptor.capture());
+        Person actualPerson = personArgumentCaptor.getValue();
+        Assert.assertEquals(actualPerson.getName(), name);
     }
+
+    @Test
+    public void removePersons() {
+        String name = "jack";
+        doNothing().when(personDao).deletePerson(any());
+        personService.removePersons(Collections.singletonList(name));
+        verify(personDao, times(1)).deletePerson(stringArgumentCaptor.capture());
+        String actualName = stringArgumentCaptor.getValue();
+        Assert.assertEquals(name, actualName);
+    }
+
+    @Test
+    public void getTopCustomer() {
+        Person expectedPerson = mockedPerson(DEFAULT_PERSON_NAME);
+        when(personDao.getTopCustomer()).thenReturn(expectedPerson);
+        Person actualPerson = personService.getTopCustomer();
+        verify(personDao, times(1)).getTopCustomer();
+        Assert.assertEquals(actualPerson.getName(), expectedPerson.getName());
+    }
+
 }
--- src/test/java/ServiceTest.java
+++ src/test/java/ServiceTest.java
@@ -1,4 +1,5 @@
 import com.application.Service;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.junit.MockitoJUnitRunner;
 
@@ -7,25 +8,8 @@ public class ServiceTest extends BaseTest {
 
     public Service service;
 
-//
-//    @Before
-//    public void setup() {
-//        this.service = new Service(redisClient);
-//    }
-//
-//    @Test
-//    public void testPassword() {
-//        String name = "name";
-//        String password = "password";
-//        service.addPassword(name, password);
-//        Assert.assertEquals(password, service.getPassword(name));
-//        Assert.assertNotEquals(null, service.getPassword(name));
-//    }
+    @Test
+    public void dummy() {
 
-//    @Test
-//    public void testSet() {
-//        Person person = mockedPerson("archit");
-//        service.addPersons(Collections.singletonList(person));
-//
-//    }
+    }
 }
